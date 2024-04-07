04e60e6980e35b00cc752d0a8bf13277e72d9cbe
Mariia Petrenko <filisinia@gmail.com>
2024-04-07 14:28:36 +0300

    feat: solve logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -171,8 +171,16 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const funcName = func.name;
+    const funcArgs = args.map((arg) => JSON.stringify(arg)).join(',');
+
+    logFunc(`${funcName}(${funcArgs}) starts`);
+    const res = func(...args);
+    logFunc(`${funcName}(${funcArgs}) ends`);
+    return res;
+  };
 }
 
 /**
