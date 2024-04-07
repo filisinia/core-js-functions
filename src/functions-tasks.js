427dd496de3c0fceb42a3f89557dc4e081e10255
Mariia Petrenko <filisinia@gmail.com>
2024-04-07 13:35:20 +0300

    feat: solve memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -114,8 +114,9 @@ function getPolynom(...coefficients) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const savedRes = func();
+  return () => savedRes;
 }
 
 /**
